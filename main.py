3b3836ed2e020d329ae2f0ea32cc1154517842b8
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-02-20 16:19:11 -0500

    update main

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+# from agents import Agent, Runner
+# from agents.mcp import MCPServerStdio, MCPServer
+# from agents import Agent, Runner, gen_trace_id, trace
+# from openai.types.responses import ResponseTextDeltaEvent
+# import asyncio
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# import OPENAI_API_KEY from .env file
+from dotenv import load_dotenv
+load_dotenv()
